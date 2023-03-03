b5445b84a9969f7c23ed25536e58289de774b19c
maikeliani <maikel.iani31@gmail.com>
2023-03-03 14:33:32 -0300

    Add project files.

---
+++ numeroPrimo/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float numero;
+        int contador = 0;
+        Console.WriteLine("Digite um numero maior que zero");
+        numero = float.Parse(Console.ReadLine());
+
+        for(int x=1; x <= numero; x++)
+        {
+            if (numero % x == 0)
+            {
+                contador++;
+            }
+
+        }
+
+        if (contador != 2)
+        {
+            Console.WriteLine(" Não é numero primo");
+        }
+        else
+        {
+            Console.WriteLine(" É numero primo!");
+        }
+    }
+}
